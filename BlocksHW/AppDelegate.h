6602eff64898844cc317caa94d26b8eef5137448
robert <robert@roberts-Mac.local>
2017-03-06 02:52:34 +0300

    Initial Commit

---
+++ BlocksHW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocksHW
+//
+//  Created by robert on 3/6/17.
+//  Copyright (c) 2017 robert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocksHW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocksHW
+//
+//  Created by robert on 3/6/17.
+//  Copyright (c) 2017 robert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
